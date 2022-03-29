cc11fac3cac2d984a61df343f87dc1ac1013edaf
viac92 <64146594+viac92@users.noreply.github.com>
2022-03-29 19:17:12 +0200

    some example of methods implementation

--- 06_methods/src/main.rs
+++ 06_methods/src/main.rs
@@ -1,3 +1,48 @@
+#[derive(Debug)]
+struct Rectangle {
+    height: u32,
+    weight: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.height * self.weight
+    }
+    fn can_hold(&self, second_rectangle: &Rectangle) -> bool {
+        if self.area() >= second_rectangle.area() {
+            true
+        } else {
+            false
+        }
+    }
+    fn square(size: u32) -> Rectangle {
+        Rectangle { 
+            height: size,
+             weight: size 
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let my_rectangle = Rectangle {
+        height: 10,
+        weight: 20,
+    };
+
+    let little_rectangle = Rectangle {
+        height: 5,
+        weight: 5,
+    };
+
+    let big_rectangle = Rectangle {
+        height: 100,
+        weight: 200,
+    };
+
+    let square = Rectangle::square(10);
+
+    println!("Area - {}", my_rectangle.area());
+    println!("Can big rectangle hold little one? - {}", big_rectangle.can_hold(&little_rectangle));
+    println!("Can a small rectangle hold big one? - {}", little_rectangle.can_hold(&big_rectangle));
+    println!("Squre area: {}", square.area());
 }
