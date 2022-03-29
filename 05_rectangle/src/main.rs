4fdb838503ea24ff394947087af13beeb432ec19
viac92 <64146594+viac92@users.noreply.github.com>
2022-03-29 17:44:51 +0200

    first commit

---
+++ 05_rectangle/src/main.rs
@@ -0,0 +1,16 @@
+struct Rectangle {
+    height: u32,
+    weight: u32,
+}
+
+fn main() {
+    let my_rectangle = Rectangle {
+        height: 10,
+        weight: 20, 
+    };
+    println!("{}", area(&my_rectangle));
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.height * rectangle.weight
+}
