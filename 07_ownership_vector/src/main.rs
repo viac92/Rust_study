280b01f9db7cf4be564c990d7de950e9102f863f
viac92 <64146594+viac92@users.noreply.github.com>
2022-04-19 14:45:56 +0200

    change comment

--- 07_ownership_vector/src/main.rs
+++ 07_ownership_vector/src/main.rs
@@ -11,15 +11,13 @@ fn main() {
     let fifth = v.pop().expect("vector empty!");
     assert_eq!(fifth, "105");
 
-    // Pop the value of a certain index and assign it to a varible, the last value of the vec take 
-    // mettendolo al suo posto.
+    // Pop the value of a certain index and assign it to a varible, the last value of the vec take the place
     let second = v.swap_remove(1);
     assert_eq!(second, "102");
 
-    // Scambio un valore con un altro nello stesso posto
+    // Change a value with another in the same place
     let third = std::mem::replace(&mut v[2], "substitute".to_string());
     assert_eq!(third, "103");
 
-    // controlliamo quello che è rimasto nel vettore
     assert_eq!(v, vec!["101", "104", "substitute"]);
 }
